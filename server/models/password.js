75f80b004e8df163e46d65efcac8cab6b835ddae
Patrick Hammon <phammon@Patricks-MBP.home>
2018-01-07 22:21:27 -0500

    add GET routes and more refactoring

--- server/models/password.js
+++ server/models/password.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 
 var Password = mongoose.model('password', {
+    name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
     username: {
         type: String,
         required: true,
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser'); //takes Json and converts to object
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Password} = require('./models/password');
@@ -18,6 +19,30 @@ app.post('/passwords', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/passwords', (req, res) => {
+     Password.find().then((passwords) =>{
+        res.send({ passwords });
+     },(e) => {
+        res.status(400).send(e);
+     })
+})
+
+app.get('/passwords/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    } 
+    Password.findById(id).then((password) => {
+        if(!password) {
+            return res.send.status(404).send();
+        }
+        res.send({password});
+    }).catch((e) => {
+        res.status(404).send(); 
+    })
 });
 
 app.listen(3000 , () => {
