19a70b6c7b104ef31205e5c71d3af7abfcbe1be8
Patrick Hammon <phammon@Patricks-MBP.home>
2018-01-14 11:46:59 -0500

    resolved package.json conflict

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var Mongoose = require('mongoose');
 
 //tell mongoose we want to use built in promis library
 Mongoose.Promise = global.Promise;
-Mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/vaulto-backend');
+Mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {Mongoose}
